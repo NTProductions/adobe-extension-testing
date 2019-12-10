941988d8d3eb0afd1950c38166ddf0c5a3cc72ca
Nate Lovell <Natelovell99@gmail.com>
2019-12-09 19:01:19 -0700

    Main Skeleton Files
    These are just the bones, follow the tutorial to fill in the missing parts

---
+++ Extension Testing/js/main.js
@@ -0,0 +1,35 @@
+(function () {
+ 'use strict';
+ var favsList = [];
+  var favsIndices = [];
+ var path, slash;
+ path = location.href;
+	if(getOS() == "MAC") {
+		slash = "/";
+		path = path.substring(0, path.length - 11);
+	}
+	if(getOS() == "WIN") {
+		slash = "/";
+		path = path.substring(8, path.length - 11);
+	}
+
+ }());
+
+function goIntoJSX() {
+	
+	}
+
+function getOS() {
+ 		var userAgent = window.navigator.userAgent,
+ 		platform = window.navigator.platform,
+ 		macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+ 		windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+ 		os = null;
+
+ 		if(macosPlatforms.indexOf(platform) != -1) {
+ 			os = "MAC";
+ 		} else if(windowsPlatforms.indexOf(platform) != -1) {
+ 			os = "WIN";
+ 		}
+ 		return os;
+ 	}
---
+++ Extension Testing/jsx/aftereffects.jsx
@@ -0,0 +1,22 @@
+﻿#include "json2.js"
+
+function getLayerNames(arg) {
+    var layerNames = [];
+    var comp = app.project.activeItem;
+    for(var i = 1; i <= comp.numLayers; i++) {
+        layerNames.push(comp.layer(i).name);
+        }
+
+    return JSON.stringify(layerNames);
+    }
+
+function osCheck() {
+        var os = $.os;
+        var match = os.indexOf("Windows");
+        if(match != (-1)) {
+                var userOS = "PC";
+            } else {
+                 var userOS = "MAC";
+                }
+            return userOS;
+    }
